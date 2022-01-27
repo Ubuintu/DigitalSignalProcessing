5401667656af6a0f6feed984c2192dc193e2fbe4
Ubuintu <bui.m.tommy@gmail.com>
2022-01-27 13:28:58 -0600

    Corrected RCV and TX filter verilog implementations to not have multiple drivers

--- 1Deliverable/RCV_filt.v
+++ 1Deliverable/RCV_filt.v
@@ -28,17 +28,7 @@ reg signed [17:0]	x[20:0];
 //always @ (posedge clk)    //for quartus
 always @ *  //for modelSim
     if (reset) begin
-        for (i=0; i<=20; i=i+1)
-            x[i] = 18'sd0;
-	for (i=0; i<=10; i=i+1)
-	    mult_out[i] = 36'sd0;
-	for (i=0; i<=10; i=i+1)
-	    sum_level_1[i] = 18'sd0;
-	for (i=0; i<=5; i=i+1)
-	    sum_level_2[i] = 18'sd0;
-	for (i=0; i<=2; i=i+1)
-	    sum_level_3[i] = 18'sd0;
-	sum_level_4 = 18'sd0;
+			x[0] = 18'sd0;
     end
     else begin
 	    x[0] = $signed( {x_in[17], x_in[17:1]} ); 
@@ -46,63 +36,95 @@ always @ *  //for modelSim
 
 //x_in[i] is 2s16
 always @ (posedge clk)
-    begin
+    if (reset) begin
         for(i=1; i<21;i=i+1)
-            x[i] <= $signed( x[i-1] );
+            x[i] <= 18'sd0;
+    end
+    else begin
+        for(i=1; i<21;i=i+1)
+            x[i] <= $signed( x[i-1] ); 
     end
 
 
 //1s17 + 1s17 will cause overflow for sum_lvl_1[i]
 always @ *
-    for(i=0;i<=9;i=i+1)
-        sum_level_1[i] = $signed(x[i])+$signed(x[20-i]);
+    if (reset) begin
+		 for(i=0;i<=9;i=i+1)
+			  sum_level_1[i] = 18'sd0;
+    end
+    else begin
+		 for(i=0;i<=9;i=i+1)
+			  sum_level_1[i] = $signed(x[i])+$signed(x[20-i]);
+    end
 
 always @ *
-    sum_level_1[10] = $signed(x[10]);
+    if (reset) sum_level_1[10] = 18'sd0;
+	 else sum_level_1[10] = $signed(x[10]);
 
 
 // always @ (posedge clk)
 always @ *
-    for(i=0;i<=10; i=i+1)
-	 //should be 2s34 (2s16*0s18)
-        mult_out[i] = $signed(sum_level_1[i]) * $signed(b[i]);
+    if (reset) begin
+		 for(i=0;i<=10; i=i+1)
+		 //should be 2s34 (2s16*0s18)
+			  mult_out[i] = 35'sd0;
+    end
+    else begin
+		 for(i=0;i<=10; i=i+1)
+		 //should be 2s34 (2s16*0s18)
+			  mult_out[i] = $signed(sum_level_1[i]) * $signed(b[i]);
+    end
 
-
-//Try sum_level_2 to 1s17 since mult_out will never be close to 1 from b[i]
+//coeffs are 0s17, x[i] is 2s16
 always @ *
-    for(i=0;i<=4;i=i+1)
-        sum_level_2[i] = $signed( mult_out[2*i][34:17] ) + $signed( mult_out[2*i+1][34:17] );
+    if (reset) begin
+		 for(i=0;i<=4;i=i+1)
+			  sum_level_2[i] = 18'sd0;
+    end
+    else begin
+		 for(i=0;i<=4;i=i+1)
+			  sum_level_2[i] = $signed( mult_out[2*i][34:17] ) + $signed( mult_out[2*i+1][34:17] );
+    end
 
 always @ *
-    sum_level_2[5] = $signed(mult_out[10][34:17]);
-
-
+    if (reset) sum_level_2[5] = 18'sd0;
+	 else sum_level_2[5] = $signed(mult_out[10][34:17]);
+    
 always @ *
-    for(i=0;i<=2;i=i+1)
-        sum_level_3[i] = $signed(sum_level_2[2*i]) + $signed(sum_level_2[2*i+1]);
+    if (reset) begin
+		 for(i=0;i<=2;i=i+1)
+			  sum_level_3[i] = 18'sd0;
+    end
+    else begin
+		 for(i=0;i<=2;i=i+1)
+			  sum_level_3[i] = $signed(sum_level_2[2*i]) + $signed(sum_level_2[2*i+1]);
+    end
 			
 always @ *
-    sum_level_4 = $signed(sum_level_3[0]) + $signed(sum_level_3[1]) + $signed(sum_level_3[2]);
+    if (reset) sum_level_4 = 18'sd0;
+	 else sum_level_4 = $signed(sum_level_3[0]) + $signed(sum_level_3[1]) + $signed(sum_level_3[2]);
+    
 
 always @ (posedge clk)
-    y = $signed(sum_level_4);
+    if (reset) y = 18'sd0;
+	 else y = $signed(sum_level_4);
 
 	
 //always @ *	//<- Don't use this especially in modelsim
 initial
    begin
 
-	//b[0] = 18'sd2817;
-	//b[1] = 18'sd4060;
-	//b[2] = 18'sd2289;
-	//b[3] = -18'sd2373;
-	//b[4] = -18'sd7348;
-	//b[5] = -18'sd8574;
-	//b[6] = -18'sd2772;
-	//b[7] = 18'sd10263;
-	//b[8] = 18'sd26830;
-	//b[9] = 18'sd40696;
-	//b[10] = 18'sd46096;
+	b[0] = 18'sd2817;
+	b[1] = 18'sd4060;
+	b[2] = 18'sd2289;
+	b[3] = -18'sd2373;
+	b[4] = -18'sd7348;
+	b[5] = -18'sd8574;
+	b[6] = -18'sd2772;
+	b[7] = 18'sd10263;
+	b[8] = 18'sd26830;
+	b[9] = 18'sd40696;
+	b[10] = 18'sd46096;
 
    end
 
--- 1Deliverable/TX_filt.v
+++ 1Deliverable/TX_filt.v
@@ -28,17 +28,7 @@ reg signed [17:0]	x[20:0];
 //always @ (posedge clk)    //for quartus
 always @ *  //for modelSim
     if (reset) begin
-        for (i=0; i<=20; i=i+1)
-            x[i] = 18'sd0;
-	for (i=0; i<=10; i=i+1)
-	    mult_out[i] = 36'sd0;
-	for (i=0; i<=10; i=i+1)
-	    sum_level_1[i] = 18'sd0;
-	for (i=0; i<=5; i=i+1)
-	    sum_level_2[i] = 18'sd0;
-	for (i=0; i<=2; i=i+1)
-	    sum_level_3[i] = 18'sd0;
-	sum_level_4 = 18'sd0;
+			x[0] = 18'sd0;
     end
     else begin
 	    x[0] = $signed( {x_in[17], x_in[17:1]} ); 
@@ -46,46 +36,81 @@ always @ *  //for modelSim
 
 //x_in[i] is 2s16
 always @ (posedge clk)
-    begin
+    if (reset) begin
         for(i=1; i<21;i=i+1)
-            x[i] <= $signed( x[i-1] );
+            x[i] <= 18'sd0;
+    end
+    else begin
+        for(i=1; i<21;i=i+1)
+            x[i] <= $signed( x[i-1] ); 
     end
 
 
 //1s17 + 1s17 will cause overflow for sum_lvl_1[i]
 always @ *
-    for(i=0;i<=9;i=i+1)
-        sum_level_1[i] = $signed(x[i])+$signed(x[20-i]);
+    if (reset) begin
+		 for(i=0;i<=9;i=i+1)
+			  sum_level_1[i] = 18'sd0;
+    end
+    else begin
+		 for(i=0;i<=9;i=i+1)
+			  sum_level_1[i] = $signed(x[i])+$signed(x[20-i]);
+    end
 
 always @ *
-    sum_level_1[10] = $signed(x[10]);
+    if (reset) sum_level_1[10] = 18'sd0;
+	 else sum_level_1[10] = $signed(x[10]);
 
 
 // always @ (posedge clk)
 always @ *
-    for(i=0;i<=10; i=i+1)
-	 //should be 2s34 (2s16*1s17)
-        mult_out[i] = $signed(sum_level_1[i]) * $signed(b[i]);
-
+    if (reset) begin
+		 for(i=0;i<=10; i=i+1)
+		 //should be 3s33 (2s16*1s17)
+			  mult_out[i] = 35'sd0;
+    end
+    else begin
+		 for(i=0;i<=10; i=i+1)
+		 //should be 3s33 (2s16*1s17)
+			  mult_out[i] = $signed(sum_level_1[i]) * $signed(b[i]);
+    end
 
 //coeffs are 1s17, x[i] is 2s16
 always @ *
-    for(i=0;i<=4;i=i+1)
-        sum_level_2[i] = $signed( mult_out[2*i][33:16] ) + $signed( mult_out[2*i+1][33:16] );
+    if (reset) begin
+		 for(i=0;i<=4;i=i+1)
+			  sum_level_2[i] = 18'sd0;
+    end
+    else begin
+		 for(i=0;i<=4;i=i+1)
+			  sum_level_2[i] = $signed( mult_out[2*i][33:16] ) + $signed( mult_out[2*i+1][33:16] );
+    end
 
 always @ *
-    sum_level_2[5] = $signed(mult_out[10][33:16]);
+    if (reset) sum_level_2[5] = 18'sd0;
+	 else sum_level_2[5] = $signed(mult_out[10][33:16]);
+    
 
 
 always @ *
-    for(i=0;i<=2;i=i+1)
-        sum_level_3[i] = $signed(sum_level_2[2*i]) + $signed(sum_level_2[2*i+1]);
+    if (reset) begin
+		 for(i=0;i<=2;i=i+1)
+			  sum_level_3[i] = 18'sd0;
+    end
+    else begin
+		 for(i=0;i<=2;i=i+1)
+			  sum_level_3[i] = $signed(sum_level_2[2*i]) + $signed(sum_level_2[2*i+1]);
+    end
 			
 always @ *
-    sum_level_4 = $signed(sum_level_3[0]) + $signed(sum_level_3[1]) + $signed(sum_level_3[2]);
+    if (reset) sum_level_4 = 18'sd0;
+	 else sum_level_4 = $signed(sum_level_3[0]) + $signed(sum_level_3[1]) + $signed(sum_level_3[2]);
+    
 
 always @ (posedge clk)
-    y = $signed(sum_level_4);
+    if (reset) y = 18'sd0;
+	 else y = $signed(sum_level_4);
+    
 
 	
 //always @ *	//<- Don't use this especially in modelsim
