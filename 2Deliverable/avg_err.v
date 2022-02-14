7e208a1d08f872ff088b2d2712e1420e80078589
Ubuintu <bui.m.tommy@gmail.com>
2022-02-13 19:37:11 -0600

    initial avg_err.v commit for step 4

---
+++ 2Deliverable/avg_err.v
@@ -0,0 +1,7 @@
+module avg_err #(parameter LFSR_WID = 22,parameter ACC_WID = 40)( 
+    input signed [17:0] dec_var,
+    input sym_clk_en, clr_acc, clk, reset, 
+    output reg signed [17:0] ref_lvl, map_out_pwr
+);
+
+endmodule
